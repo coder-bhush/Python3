445d7006aa5d505c78cd0699c8fc9588650c4759
Bhushan <bhushanjarhad03@gmail.com>
2023-10-07 10:31:27 -0700

    Beginning of Python

---
+++ intro.py
@@ -0,0 +1,49 @@
+
+#datatypes and variable declaration..
+
+a = 'Hello' # string
+b = 5       # int 
+c = 15.20   # float 
+d = True    # Bool
+e = 3j      #complex
+f = 5j      #complex
+
+# To print an Variables we have to use print()
+
+print(a)
+print(b)
+print(c)
+
+
+# We have to store multiple lines in one variable then
+# we have to write those lines in tripple quotes 
+ 
+g = """Track your progress with the free "My Learning" program here  at W3Schools.
+        Log in to your account, and start earning points!
+        This is an optional feature. You can study W3Schools without using
+        My Learning"""
+
+
+# We can also Know the data type of an variable using type()
+
+print(type(d))
+print(type(e))
+print(type(f))
+
+
+# Type Casting Method 
+# Type casting of variables : 
+
+m = 15.20       # m having float value
+n = 10          # n having int value 
+print(int(m))
+print(float(n))
+                # we convert its type using Type Casting Method..
+
+
+'''Multiline comments in python 
+    are as Follows : '''
+
+''' My name is Bhushan '''
+""" She is My Sister her name is Sanjivanee """
+
